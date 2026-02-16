41fc05dd4fd32f6b866d3703765ad228d05b9947
bkht17 <bahytt05@gmail.com>
2026-02-16 15:48:28 +0500

    add blog migrations and register blog models

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'author', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('title', 'slug', 'body')
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('body',)
